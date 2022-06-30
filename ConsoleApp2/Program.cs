82b00fed3656606d92145265be5af7e16bca881c
RomaGulis <108432937+RomaGulis@users.noreply.github.com>
2022-06-30 17:21:55 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, userWork, userSignOfTheZodiac;
+            int userAge;
+            
+            Console.WriteLine("Как вас зовут?");
+            userName = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Какой у вас знак зодиака?");
+            userSignOfTheZodiac = Console.ReadLine();
+            Console.WriteLine("Где вы работаете?");
+            userWork = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {userName}, вам {userAge} год, вы {userSignOfTheZodiac} и работаете на {userWork}.");
+        }
+    }
+}
